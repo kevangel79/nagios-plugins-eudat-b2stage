6f91afb21c35c8b17e6f9f5fab4828bfcfb77d7d
Mattia <m.dantonio81@gmail.com>
2018-10-25 09:58:31 +0200

    BUG fix: syntax errors in print Fixes in code formatting

--- check_b2stage_http-api.py
+++ check_b2stage_http-api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import sys, requests
+import sys
+import requests
 import argparse
 
 # ##############################################################################
@@ -26,20 +27,20 @@ def ValidateValues(arguments):
             print_help()
             exit()
 
+
 def print_help():
         """ Print help values."""
 
-        print "usage: check_b2stage_http-api.py -H -p"
-        print "--- ---- ---- ---- ---- ---- ----\n"
-        print "main arguments:"
-        print "-H hostname"
-        print "\n"
-        print "optional arguments:"
-        print " -h, --help  show this help message and exit"
-        print "-p port"
-        print "-t timeout"
-        print "-v verbose"
-
+        print("usage: check_b2stage_http-api.py -H -p")
+        print("--- ---- ---- ---- ---- ---- ----\n")
+        print("main arguments:")
+        print("-H hostname")
+        print("\n")
+        print("optional arguments:")
+        print(" -h, --help  show this help message and exit")
+        print("-p port")
+        print("-t timeout")
+        print("-v verbose")
 
 
 def debugValues(arguments):
@@ -48,19 +49,20 @@ def debugValues(arguments):
             arguments: the input arguments
     """
     if arguments.debug:
-        print "[debugValues] - hostname:"+ arguments.hostname
-    if arguments.port!='':
-        print "[debugValues] - port:" + arguments.port
-    if arguments.timeout!='':
-        print "[debugValues] - timeout:" + str(arguments.timeout)
-    if arguments.t!='':
-        print "[debugValues] - timeout:" + str(arguments.t)
+        print("[debugValues] - hostname: %s" % arguments.hostname)
+    if arguments.port != '':
+        print("[debugValues] - port: %s" % arguments.port)
+    if arguments.timeout != '':
+        print("[debugValues] - timeout: %s" % arguments.timeout)
+    if arguments.t != '':
+        print("[debugValues] - timeout: %s" % arguments.t)
 
-def checkHealth(URL,timeout):
+
+def checkHealth(URL, timeout):
     """ Check service status.
         Args:
            URL : service hostname
-           timeout : how long should we wati 
+           timeout : how long should we wati
     """
     try:
         out = requests.get(url=URL, timeout=timeout)
@@ -86,15 +88,17 @@ def checkHealth(URL,timeout):
     exit_code = 3
     return description, exit_code
 
+
 def printResult(description, exit_code):
-    """ Print the predefined values 
+    """ Print the predefined values
         Args:
             description: the nagios description
             exit_code: the code that should be returned to nagios
     """
 
-    print 'description'
+    print(description)
     sys.exit(exit_code)
+
 
 def main():
 
@@ -103,21 +107,20 @@ def main():
     parser.add_argument("--hostname", "-H", help='The Hostname of B2STAGE service')
     parser.add_argument("--port", "-p", type=int)
     parser.add_argument("--timeout", "-t", metavar="seconds", help="Timeout in seconds. Must be greater than zero", type=int, default=30)
-    parser.add_argument("--verbose","-v", dest='debug', help='Set verbosity level', action='count', default=0)
+    parser.add_argument("--verbose", "-v", dest='debug', help='Set verbosity level', action='count', default=0)
     arguments = parser.parse_args()
     ValidateValues(arguments)
     NAGIOS_RESULT = 0
 
-    URL=arguments.hostname
+    URL = arguments.hostname
     if arguments.port is not None:
-       URL += ":%s" % arguments.port
+        URL += ":%s" % arguments.port
 
     URL += "/api/status"
     timeout = arguments.timeout
-    description, exit_code = checkHealth(URL,timeout)
+    description, exit_code = checkHealth(URL, timeout)
     printResult(description, exit_code)
 
 
 if __name__ == "__main__":
     main()
-
